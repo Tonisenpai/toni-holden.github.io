4852391d9ef5ad3fa341dfd9696f622ae140254c
Antonia Holden <tonitechtalk@gmail.com>
2021-12-14 04:24:08 +0000

    did parts 1-6 of data.js in matchy

--- projects/matchy/data.js
+++ projects/matchy/data.js
@@ -12,19 +12,50 @@
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+// create variable named object and assign it to object literal
+var animal = {};
+
+// create key and value for "species" using dot notation
+animal.species = 'pig';
+// create key and value for "name" using bracket notation
+animal['name'] = 'Andrew';
+// create key for noises and initialize with array literal
+animal.noises = [];
+
+// console.log "animal"
+console.log(animal);
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+// create the variable "noises" and set to array literal
+var noises = [];
+
+// use bracket notation to add element to array
+noises[0] = 'oink';
+// use array function to add another noise
+noises.push('grunt');
+// use unshift function to place another noise to beginning of array
+noises.unshift('snort');
+// use length property to add a noise to the end of the array
+noises[noises.length] = 'squeal';
+
+console.log(noises.length);
+console.log(noises[noises.length - 1]);
+console.log(noises);
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+// assign the 'noises' property on 'animal' to noises array
+animal.noises = noises;
 
+// add another noise to 'noises' property on animal
+animal.noises.push('chomp');
 
 /* *******************************************************************
  * Step 4 - Review
@@ -49,7 +80,36 @@
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+// create the variable 'animals' and assign to array literal
+var animals = [];
 
+// push the 'animal' object to the animals array
+animals.push(animal);
+
+// create variable 'duck' and assign it specific values
+var duck = {
+  species: 'duck',
+  name: 'Jerome',
+  noises: ['quack', 'honk', 'sneeze', 'woosh']
+};
+
+// push duck into animals array
+animals.push(duck);
+
+// create 2 more animal objects and push them into animals array
+var dog = {
+  species: 'dog',
+  name: 'Perry',
+  noises: ['bark', 'growl', 'whimper', 'pant']
+};
+
+var horse = {
+  species: 'horse',
+  name: 'Carey',
+  noises: ['neigh', 'whinny', 'snort', 'nicker']
+};
+
+animals.push(dog, horse);
 
 //////////////////////////////////////////////////////////////////////
 // Step 7 - Making Friends ///////////////////////////////////////////
