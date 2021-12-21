bcb12b8b966c4ea457b04c97301e8da3c5c6e8bf
Antonia Holden <tonitechtalk@gmail.com>
2021-12-21 05:38:11 +0000

    added to Studies: Variables page

--- Studies/variables.js
+++ Studies/variables.js
@@ -5,10 +5,21 @@
  * tied to a value, but if you try to call it later, the value will return "undefined".
  * These variables are later used in statements, expressions and functions.
  * 1. For a variable to exist, they must first be declared. The declaration of a 
- * functionis how your code knows of the existence of your variable. There is a specific
+ * function is how your code knows of the existence of your variable. There is a specific
  * syntax used to declare a variable. To begin, you must use one of three keywords:
  * "var", "let", or "const". Next, you must name the variable (using camelCase). To
  * complete your declaration, you end your expression with a semicolon.
- * 2. To add a value to a variable 
- * 3.
+ * 2. To add a value to a variable, it must be "assigned" to the variable. Assignment is 
+ * done by using what is called an assignment operator (=). Variables can also be 
+ * "initialized" during declaration. A variable being initialized just means that it is
+ * declared and assigned all in one line of code. This is recommended for most situations.  
+ * 3. Variables can also be re-assigned, under normal circumstances (within its own scope). 
+ * There are some instances where this is not the case, but basically, they can be 
+ * re-assigned when using the "var" keyword. In cases where the keywords "let" and "const"
+ * are concerned, this is where you'll run into some issues with re-assignment. This has
+ * a lot to do with the concept mentioned above called "scope". Once assigned, variables
+ * assigned or initialized with the keyword "const" cannot be re-assigned throughout the 
+ * life of your program. Let variables are block/function scoped, which means that they
+ * cannot be accessed outside of the function it was declared in.
+ * 4. 
  */
